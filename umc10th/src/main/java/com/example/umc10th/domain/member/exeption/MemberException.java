b364a5945b92cfabf0acc420094a5e9c529cd95d
taerimiiii <shipton0201@gmail.com>
2026-04-06 21:07:06 +0900

    feat: 사용자미션, 사용자 폴더 구조 세팅

---
+++ umc10th/src/main/java/com/example/umc10th/domain/member/exeption/MemberException.java
@@ -0,0 +1,7 @@
+package com.example.umc10th.domain.member.exeption;
+
+public class MemberException extends RuntimeException {
+    public MemberException(String message) {
+        super(message);
+    }
+}
---
+++ umc10th/src/main/java/com/example/umc10th/domain/mission/exception/MemberMissionExeption.java
@@ -0,0 +1,7 @@
+package com.example.umc10th.domain.mission.exception;
+
+public class MemberMissionExeption extends RuntimeException {
+    public MemberMissionExeption(String message) {
+        super(message);
+    }
+}
