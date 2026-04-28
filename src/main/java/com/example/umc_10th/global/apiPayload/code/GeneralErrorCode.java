00a1c091740c0c16651a2028dc75d0ee4099e4b0
cdth12345 <cdth7509@naver.com>
2026-04-28 17:50:03 +0900

    feat: 에러 핸들링 초안 설계

--- src/main/java/com/example/umc_10th/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/com/example/umc_10th/global/apiPayload/code/GeneralErrorCode.java
@@ -20,7 +20,10 @@ public enum GeneralErrorCode implements BaseErrorCode{
     NOT_FOUND(HttpStatus.NOT_FOUND,
             "COMMON404_1",
             "해당 리소스를 찾을 수 없습니다."
-    ),;
+    ),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
+            "COMMON500_1",
+            "서버 오류가 발생했습니다.");
 
 
     private final HttpStatus status;
